eb7b32133189c20e22d75ddb59acbb3317453d2e
luismr-educarex <luismr@educarex.es>
2023-10-08 13:05:01 +0200

    Primer commit

--- src/unidad1/ejemplos/Aplicacion.java
+++ src/unidad1/ejemplos/Aplicacion.java
@@ -14,7 +14,7 @@ public class Aplicacion {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Esto es una prueba con el repositorio");
     }
     
 }
